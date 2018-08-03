d9a30a48a646471f93a9c60fd469f1f592417a10
DRFH <DavidRFHarris@gmail.com>
2018-08-03 15:33:10 -0400

    Added test program file.

---
+++ tests/test1.c
@@ -0,0 +1,21 @@
+//
+//  test1.c
+//  libpeers
+//
+//  Created by David Harris on 08/03/2018.
+//  Copyright (c) 2018 MegaApps. All rights reserved.
+//
+
+#include <stdio.h>
+#include <libpeers.h>
+
+int main(int argc,char const *argv[])
+{
+	printf("Test 1 - v0.1\n");
+	printf("libPeers\n");
+	printf("-----------------\n");
+
+
+	printf("Exiting...\n");
+	return 0;
+}
