9c93ce59cbbb8041b9b90c252c67d206d471a633
DRFH <DavidRFHarris@gmail.com>
2018-08-03 14:25:09 -0400

    Create global.h header file.

---
+++ src/global.h
@@ -0,0 +1,23 @@
+//
+//  global.h
+//  libpeers
+//
+//  Created by David Harris on 11/16/18.
+//  Copyright (c) 2011 MegaApps. All rights reserved.
+//
+
+
+#ifndef libpeers_global_h
+#define libpeers_global_h
+
+#define		kKB(x)		(x*1024)
+#define		kMB(x)		(x*kKB(1000))
+#define		kGB(x)		(x*kMB(1000))
+
+#define		kVersion	"1.0"
+
+
+
+
+
+#endif
