47deda2fb9320197886b63bf39581cac1eb49ce0
DRFH <DavidRFHarris@gmail.com>
2018-08-06 16:54:49 -0400

    initial sock_t type

--- src/socks.c
+++ src/socks.c
@@ -7,4 +7,66 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <unistd.h>
+
 #include "socks.h"
+
+
+sock_t*	sock_init(void)
+{
+	sock_t		*s=malloc(sizeof(sock_t));
+
+	assert(s==NULL);
+	if ((s->sock=socket(AF_INET,SOCK_STREAM,0))<0)
+	{
+		printf("\n Socket creation error \n");
+		s->status=-1;
+		sock_free(s);
+	}
+	else
+	{
+		s->status=0;
+		s->port=0;
+//		memset(&s->addr, '0', sizeof(s->addr));
+	}
+	return s;
+}
+
+void sock_free(sock_t *s)
+{
+	sock_disconnect(s);
+	free(s);
+}
+
+void sock_setaddress(char* address)
+{
+	;
+}
+
+void sock_connect(sock_t *s)
+{
+	if (connect(s->sock,(struct sockaddr *)&s->addr,sizeof(s->addr))<0)
+	{
+		printf("\nConnection Failed \n");
+	}
+}
+
+void sock_disconnect(sock_t *s)
+{
+	if(s->sock>1)
+	{
+		close(s->sock);
+	}
+}
+
+void sock_send(sock_t *s,char* buff,long len)
+{
+
+}
+
+void sock_receive(sock_t *s,char* buff,long *size)
+{
+
+}
--- src/socks.h
+++ src/socks.h
@@ -9,9 +9,36 @@
 #ifndef libpeers_socks_h
 #define libpeers_socks_h
 
+#include <stdint.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <netinet/in.h>
+
+struct sock_s
+{
+	struct sockaddr_in	addr;
+	uint32_t			port;
+	int					sock;
+
+	int					status;
+};
+
+typedef	struct sock_s	sock_t;
 
 
+sock_t*	sock_init(void);
+void sock_free(sock_t *s);
 
+void sock_setaddress(char* address);
+
+void sock_connect(sock_t *s);
+void sock_disconnect(sock_t *s);
+
+void sock_send(sock_t *s,char* buff,long len);
+
+void sock_receive(sock_t *s,char* buff,long *size);
 
 
 
