a2ebf711ab8ceb0a7ebb465dd1fe643d8da5b6b7
DRFH <DavidRFHarris@gmail.com>
2018-08-07 09:05:34 -0400

    corrected #ifndef libpeers_node_h for node.h header

--- src/node.h
+++ src/node.h
@@ -1,15 +1,19 @@
 //
-//  socks.h
+//  node.h
 //  libpeers
 //
 //  Created by David Harris on 11/16/11.
 //  Copyright (c) 2011 MegaApps. All rights reserved.
 //
 
-#ifndef libpeers_socks_h
-#define libpeers_socks_h
+#ifndef libpeers_node_h
+#define libpeers_node_h
+
+#include "socks.h"
 
 #include <stdio.h>
+#include <stdint.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
