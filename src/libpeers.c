2714ca4392cbc0dbda554ad45163d226085d38f7
DRFH <DavidRFHarris@gmail.com>
2018-09-14 16:40:53 -0400

    Basic structure code added to libpeers Initial components of pthread added.

--- src/libpeers.c
+++ src/libpeers.c
@@ -11,6 +11,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
+#include <stdbool.h>
+
+#include <errno.h>
+
+#include <pthread.h>
+#include <sched.h>
+#include <unistd.h>
+
+
+/** Private declarations **/
+void libpeers_default_handle_message(p2p_ctx* ctx,char* m,node_t* n);
+void *p2p_main_loop(void* arg1);
+
+/** end **/
 
 
 p2p_ctx* p2p_init(char* ip_bind,uint32_t port)
@@ -18,7 +33,26 @@ p2p_ctx* p2p_init(char* ip_bind,uint32_t port)
 	p2p_ctx		*ctx=malloc(sizeof(p2p_ctx));
 
 	if(ctx!=NULL)
-		ctx->nodes=arraylist_new(20);
+	{
+		pthread_mutex_init(&ctx->mutex,NULL);
+		ctx->peers=arraylist_new(20);
+		ctx->handler=libpeers_default_handle_message;
+		ctx->port=port;
+		ctx->s=sock_init(AF_INET,SOCK_DGRAM,0);
+		ctx->packet_size=kKB4;
+
+	//	sock_setaddress(ctx->s,ip_bind);
+		sock_setaddress_var(ctx->s,ctx->port,htonl(INADDR_ANY));
+	//	int		opt=1;
+	//	sock_setopt(ctx->s,SOL_SOCKET,SO_REUSEADDR,&opt,1);
+	//	sock_setopt(ctx->s,SOL_SOCKET,SO_REUSEPORT,&opt,1);
+
+//		struct timeval timeout={1,0};
+//		timeout.tv_sec = 1;
+//		timeout.tv_usec = 0;
+//		sock_setopt(ctx->s,SOL_SOCKET,SO_RCVTIMEO,&timeout,sizeof(timeout));
+		ctx->status=0;
+	}
 	return ctx;
 }
 
@@ -26,10 +60,130 @@ void p2p_free(p2p_ctx *ctx)
 {
 	if(ctx!=NULL)
 	{
-		if(ctx->nodes!=NULL)
+		int		thread_status=0;
+
+		pthread_mutex_lock(&ctx->mutex);
+		ctx->status=0;
+		pthread_mutex_unlock(&ctx->mutex);
+		thread_status=pthread_join(ctx->tid,NULL);
+		printf("thread joined with status: %d",thread_status);
+		if(ctx->peers!=NULL)
 		{
-			arraylist_free(ctx->nodes);
+			arraylist_free(ctx->peers);
 		}
+		pthread_mutex_destroy(&ctx->mutex);
 		free(ctx);
 	}
+}
+
+
+
+void p2p_start(p2p_ctx *ctx)
+{
+	/* check to see that we can create listen socket */
+	if(ctx->s==NULL)
+	{
+		perror("Cannot create socket");
+		exit(1);
+	}
+
+	/* Now bind to the socket	*/
+	sock_bind(ctx->s);
+	if(ctx->s==NULL)
+	{
+		perror("Cannot bind to the socket");
+		exit(1);
+	}
+
+//	peer_t	*peer=NULL;
+
+	ctx->status=1;
+	pthread_create(&ctx->tid,NULL,p2p_main_loop,ctx);
+}
+
+void *p2p_main_loop(void* arg1)
+{
+	p2p_ctx *ctx=(p2p_ctx*)arg1;
+	sock_t	*peer_sock;
+	char	*buff=NULL;
+//	long	buff_size=0;
+	bool	local_test=true;
+
+	buff=malloc(kKB(ctx->packet_size));
+	while(local_test==true)
+	{
+		pthread_mutex_lock(&ctx->mutex);
+		if(ctx->status!=1)
+			local_test=false;
+
+	//	buff_size=0;
+	//	bzero(buff,ctx->packet_size);
+
+		pthread_mutex_unlock(&ctx->mutex);
+
+		peer_sock=sock_accept(ctx->s);
+
+	//	sock_receive(ctx->s,buff,&buff_size);
+	//	*sock_receive_message(ctx->s,buff,&buff_size);
+
+	/*	if(peer_sock!=NULL)
+		{
+			printf("peer_sock: %p  \n",peer_sock);
+			sock_free(peer_sock);
+		}
+		else
+		{
+			printf("sock_accept returned NULL peer.\n");
+		}
+	*/
+		free(peer_sock);
+		if(sched_yield())
+			fprintf(stderr,"sched_yield returned non -1: errno=%d\n",errno);
+	}
+	free(buff);
+	return NULL;
+}
+
+void p2p_receive_message(sock_t s)
+{
+	;
+}
+
+void p2p_add_peers(p2p_ctx *ctx,char* address)
+{
+	p2p_add_peers_op(ctx,address,kOneTry);
+}
+
+void p2p_add_peers_op(p2p_ctx *ctx,char* address,int option)
+{
+	;
+}
+
+void p2p_reg_message_handle(p2p_ctx *ctx,p2p_message_handle func)
+{
+	ctx->handler=func;
+}
+
+char* p2p_get_stat(p2p_ctx *ctx,char* stat_str)
+{
+	char		*ret=malloc(128);
+
+	if(strcmp(stat_str,"IP Address")==0)
+	{
+		sprintf(ret,"127.0.0.1");
+	}
+	else if(strcmp(stat_str,"PORT")==0)
+	{
+		sprintf(ret,"%d",ctx->port);
+	}
+	else if(strcmp(stat_str,"Number Of Peers")==0)
+	{
+		sprintf(ret,"%d",ctx->peers->length);
+	}
+	return ret;
+}
+
+void libpeers_default_handle_message(p2p_ctx* ctx,char* m,node_t* n)
+{
+	printf("libpeers_default_handle_message: called\n");
 }
--- src/libpeers.h
+++ src/libpeers.h
@@ -19,14 +19,27 @@ extern "C" {
 #include "node.h"
 
 #include <stdint.h>
+#include <pthread.h>
+
+
+typedef	struct p2p_s	p2p_ctx;
+
+typedef void (*p2p_message_handle)(p2p_ctx* ctx,char* m,node_t* n);
 
 struct p2p_s
 {
-	ArrayList	*nodes;
+	long		status;
+	ArrayList	*peers;
 	uint32_t	port;
+	sock_t		*s;
 
+	long		packet_size;
+
+	pthread_mutex_t	mutex;
+	pthread_t	tid;
+
+	p2p_message_handle	handler;
 };
-typedef	struct p2p_s	p2p_ctx;
 
 
 /**
@@ -44,13 +57,63 @@ p2p_ctx* p2p_init(char* ip_bind,uint32_t port);
 /**
  * Frees the allcated memort of ctx after disconnecting and releasing the nodes.
  * @param ctx		ctx that needs to be deallocated and freed.
- * @return			Returnes a pointer to memory allocated with the size of the p2p_s
- *					Also initializes ->nodes with array list
  *
  * @see p2p_free
  */
 void p2p_free(p2p_ctx *ctx);
 
+
+/**
+ * Start the pthread for the poll runloop.
+ * @param ctx		Context record of the p2p server that is to be operated on.
+ *
+ * @see p2p_start
+ */
+void p2p_start(p2p_ctx *ctx);
+
+
+
+enum peer_options { kOneTry=0 };
+
+/**
+ * Adds a peer to the list of peers.
+ * @param ctx		Context record of the p2p server that is to be operated on.
+ * @param address	Address of the peer to add into nodes list.
+ *
+ *
+ * @see p2p_add_peers
+ */
+
+void p2p_add_peers(p2p_ctx *ctx,char* address);
+void p2p_add_peers_op(p2p_ctx *ctx,char* address,int option);
+
+
+
+void p2p_reg_message_handle(p2p_ctx *ctx,p2p_message_handle func);
+
+
+/**
+ * Query the p2p context for internal stats.
+ * @param ctx		Context record of the p2p server that is to be operated on.
+ * @param stat_str	Name of the stat to lookup.
+ *
+ * @return char* string of the result
+ * @see p2p_get_stat
+ */
+
+char* p2p_get_stat(p2p_ctx *ctx,char* stat_str);
+//void p2p_get_stat(p2p_ctx *ctx,char* stat_str,char* reply);
+
+
+
+/**
+ * Query the p2p context for internal stats.
+ * @param ctx		Context record of the p2p server that is to be operated on.
+ * @param return_string	ptr to memort that will store the address that p2p_ctx is bound to.
+ *
+ * @see p2p_details_get_address
+ */
+void p2p_details_get_address(p2p_ctx *ctx,char* return_string);
 
 #ifdef __cplusplus
 }
